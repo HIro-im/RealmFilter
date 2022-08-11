eb5afbe86da24910717a74eec3f06190fff5ff0a
Hiro-im <doraemon.for.private@gmail.com>
2022-08-12 05:43:55 +0900

    登録件数の確認処理の追加

--- RealmFilter/ViewController.swift
+++ RealmFilter/ViewController.swift
@@ -12,20 +12,57 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var selectedSegment: UISegmentedControl!
+    @IBOutlet weak var selectedSegment: UISegmentedControl!    
+    @IBOutlet weak var alertText: UILabel!
     
     let realm = try! Realm()
+    
+    var currentLeftDataCount: Int = 0
+    var currentRightDataCount: Int = 0
+    let saveLimit: Int = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let allData = realm.objects(Filter.self)
         print("すべてのデータ\(allData)")
         
+        currentCounterUpdate()
         
     }
 
+    func currentCounterUpdate() {
+        let leftData = realm.objects(Filter.self).filter("selectedButton == %@", 0)
+        print("左のデータ\(leftData)")
+        currentLeftDataCount = leftData.count
+        print(currentLeftDataCount)
+        
+        let rightData = realm.objects(Filter.self).filter("selectedButton == %@", 1)
+        print("右のデータ\(rightData)")
+        currentRightDataCount = rightData.count
+        print(currentRightDataCount)
+
+        
+    }
     
     @IBAction func registerButton(_ sender: Any) {
+        
+        switch selectedSegment.selectedSegmentIndex {
+        case Timing.Lunch.rawValue:
+            if currentLeftDataCount >= saveLimit {
+                alertText.text = "登録限界です： \(selectedSegment.titleForSegment(at: selectedSegment.selectedSegmentIndex)!)"
+                return
+            }
+            
+        case Timing.Dinner.rawValue:
+            if currentRightDataCount >= saveLimit {
+                alertText.text = "登録限界です： \(selectedSegment.titleForSegment(at: selectedSegment.selectedSegmentIndex)!)"
+                return
+            }
+            
+        default:
+            print("another")
+        }
+        
         let user = Filter()
         user.text = textField.text!
         user.selectedButton = selectedSegment.selectedSegmentIndex
@@ -36,13 +73,9 @@ class ViewController: UIViewController {
         textField.text = ""
         let allData = realm.objects(Filter.self)
         print("すべてのデータ\(allData)")
-
-        let leftData = realm.objects(Filter.self).filter("selectedButton == %@", 0)
-        print("左のデータ\(leftData)")
         
-        let rightData = realm.objects(Filter.self).filter("selectedButton == %@", 1)
-        print("右のデータ\(rightData)")
-
+        currentCounterUpdate()
+        
     }
     
     
